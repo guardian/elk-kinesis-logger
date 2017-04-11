ebab10bd728a01d66ed6aa413d71f969c15f86af
Gareth Trufitt <gareth.trufitt@guardian.co.uk>
2017-04-11 10:15:29 +0100

    Add prettier formatting changes

--- src/elk-kinesis-logger.js
+++ src/elk-kinesis-logger.js
@@ -1,111 +1,115 @@
 const AWS = require('aws-sdk');
 
 class ELKKinesisLogger {
-  constructor({stage, stack, app, roleArn, streamName, verbose = true}) {
-    this.stage = stage;
-    this.stack = stack;
-    this.app = app;
-    this.roleArn = roleArn;
-    this.streamName = streamName;
-    this.verbose = verbose;
-  }
-
-  get _name() {
-    return this.constructor.name;
-  }
-
-  open() {
-    return new Promise((resolve, reject) => {
-      const sts = new AWS.STS();
-
-      const options = {
-        RoleArn: this.roleArn,
-        RoleSessionName: this.app
-      };
-
-      sts.assumeRole(options, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          this.kinesis = new AWS.Kinesis({
-            accessKeyId: data.Credentials.AccessKeyId,
-            secretAccessKey: data.Credentials.SecretAccessKey,
-            sessionToken: data.Credentials.SessionToken
-          });
-
-          this._logLines = [];
-
-          resolve(this);
-        }
-      });
-    });
-  }
-
-  close() {
-    this._consoleLog(`ensuring ${this._logLines.length} log lines are written`);
-    return Promise.all(this._logLines);
-  }
-
-  log(message, extraDetail) {
-    this._ensureOpened();
-    this._logLines.push(this._putRecord({level: 'INFO', message, extraDetail}));
-  }
-
-  error(message, extraDetail) {
-    this._ensureOpened();
-    this._logLines.push(
-      this._putRecord({level: 'ERROR', message, extraDetail})
-    );
-  }
-
-  _ensureOpened() {
-    if (!this._logLines) {
-      throw `${this._name} has not been opened. Be sure to call .open() first.`;
+    constructor({stage, stack, app, roleArn, streamName, verbose = true}) {
+        this.stage = stage;
+        this.stack = stack;
+        this.app = app;
+        this.roleArn = roleArn;
+        this.streamName = streamName;
+        this.verbose = verbose;
     }
-  }
 
-  _consoleLog(message) {
-    if (this.verbose) {
-      // eslint-disable-next-line no-console
-      console.log(
-        `${new Date().toISOString()} [INFO] from ${this._name} - ${message}`
-      );
+    get _name() {
+        return this.constructor.name;
     }
-  }
 
-  _putRecord({level, message, extraDetail = {}}) {
-    return new Promise((resolve, reject) => {
-      const coreLogMessage = {
-        stack: this.stack,
-        stage: this.stage,
-        app: this.app,
-        timestamp: new Date()
-      };
+    open() {
+        return new Promise((resolve, reject) => {
+            const sts = new AWS.STS();
 
-      const fullMsg = Object.assign(
-        {},
-        coreLogMessage,
-        {level, message},
-        extraDetail
-      );
+            const options = {
+                RoleArn: this.roleArn,
+                RoleSessionName: this.app
+            };
 
-      this._consoleLog(`writing to kinesis: ${JSON.stringify(fullMsg)}`);
+            sts.assumeRole(options, (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    this.kinesis = new AWS.Kinesis({
+                        accessKeyId: data.Credentials.AccessKeyId,
+                        secretAccessKey: data.Credentials.SecretAccessKey,
+                        sessionToken: data.Credentials.SessionToken
+                    });
 
-      const options = {
-        StreamName: this.streamName,
-        PartitionKey: 'logs',
-        Data: JSON.stringify(fullMsg)
-      };
+                    this._logLines = [];
 
-      this.kinesis.putRecord(options, err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(fullMsg);
+                    resolve(this);
+                }
+            });
+        });
+    }
+
+    close() {
+        this._consoleLog(
+            `ensuring ${this._logLines.length} log lines are written`
+        );
+        return Promise.all(this._logLines);
+    }
+
+    log(message, extraDetail) {
+        this._ensureOpened();
+        this._logLines.push(
+            this._putRecord({level: 'INFO', message, extraDetail})
+        );
+    }
+
+    error(message, extraDetail) {
+        this._ensureOpened();
+        this._logLines.push(
+            this._putRecord({level: 'ERROR', message, extraDetail})
+        );
+    }
+
+    _ensureOpened() {
+        if (!this._logLines) {
+            throw `${this._name} has not been opened. Be sure to call .open() first.`;
         }
-      });
-    });
-  }
+    }
+
+    _consoleLog(message) {
+        if (this.verbose) {
+            // eslint-disable-next-line no-console
+            console.log(
+                `${new Date().toISOString()} [INFO] from ${this._name} - ${message}`
+            );
+        }
+    }
+
+    _putRecord({level, message, extraDetail = {}}) {
+        return new Promise((resolve, reject) => {
+            const coreLogMessage = {
+                stack: this.stack,
+                stage: this.stage,
+                app: this.app,
+                timestamp: new Date()
+            };
+
+            const fullMsg = Object.assign(
+                {},
+                coreLogMessage,
+                {level, message},
+                extraDetail
+            );
+
+            this._consoleLog(`writing to kinesis: ${JSON.stringify(fullMsg)}`);
+
+            const options = {
+                StreamName: this.streamName,
+                PartitionKey: 'logs',
+                Data: JSON.stringify(fullMsg)
+            };
+
+            this.kinesis.putRecord(options, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(fullMsg);
+                }
+            });
+        });
+    }
 }
 
 module.exports = ELKKinesisLogger;
--- test/elk-kinesis-logger.js
+++ test/elk-kinesis-logger.js
@@ -4,234 +4,240 @@ const MockDate = require('mockdate');
 const ELKKinesisLogger = require('../src/elk-kinesis-logger');
 
 describe('ELKKinesisLogger', () => {
-  const config = {
-    stage: 'TEST',
-    stack: 'elk-kinesis-logger',
-    app: 'elk-kinesis-logger-tests',
-    roleArn: 'test-role',
-    streamName: 'test-stream',
-    verbose: false
-  };
+    const config = {
+        stage: 'TEST',
+        stack: 'elk-kinesis-logger',
+        app: 'elk-kinesis-logger-tests',
+        roleArn: 'test-role',
+        streamName: 'test-stream',
+        verbose: false
+    };
 
-  const logMsg = 'testing testing, is this thing on?';
+    const logMsg = 'testing testing, is this thing on?';
 
-  const date = new Date(2017, 0, 1);
+    const date = new Date(2017, 0, 1);
 
-  before(() => {
-    MockDate.set(date);
+    before(() => {
+        MockDate.set(date);
 
-    AWS.mock('STS', 'assumeRole', (params, callback) => {
-      callback(null, {
-        Credentials: {
-          AccessKeyId: 'secretID',
-          SecretAccessKey: 'secretKey',
-          SessionToken: 'secretToken'
+        AWS.mock('STS', 'assumeRole', (params, callback) => {
+            callback(null, {
+                Credentials: {
+                    AccessKeyId: 'secretID',
+                    SecretAccessKey: 'secretKey',
+                    SessionToken: 'secretToken'
+                }
+            });
+        });
+
+        AWS.mock('Kinesis', 'putRecord', (params, callback) => {
+            callback(null, {});
+        });
+    });
+
+    after(() => {
+        MockDate.reset();
+        AWS.restore();
+    });
+
+    it('should raise an error if not opened', () => {
+        const logger = new ELKKinesisLogger(config);
+
+        try {
+            logger.log('test');
+        } catch (actual) {
+            assert.equal(
+                actual,
+                'ELKKinesisLogger has not been opened. Be sure to call .open() first.'
+            );
         }
-      });
     });
 
-    AWS.mock('Kinesis', 'putRecord', (params, callback) => {
-      callback(null, {});
+    it('should write a simple log to kinesis', done => {
+        const logger = new ELKKinesisLogger(config);
+
+        logger.open().then(() => {
+            logger.log(logMsg);
+
+            logger
+                .close()
+                .then(actual => {
+                    const expected = [
+                        {
+                            stack: 'elk-kinesis-logger',
+                            stage: 'TEST',
+                            app: 'elk-kinesis-logger-tests',
+                            timestamp: date,
+                            level: 'INFO',
+                            message: logMsg
+                        }
+                    ];
+
+                    const kinesisMsg = {
+                        StreamName: config.streamName,
+                        PartitionKey: 'logs',
+                        Data: JSON.stringify(expected[0])
+                    };
+
+                    assert.equal(true, logger.kinesis.putRecord.calledOnce);
+                    assert.equal(
+                        true,
+                        logger.kinesis.putRecord.calledWith(kinesisMsg)
+                    );
+                    assert.deepEqual(actual, expected);
+                    done();
+                })
+                .catch(err => done(new Error(err)));
+        });
     });
-  });
 
-  after(() => {
-    MockDate.reset();
-    AWS.restore();
-  });
+    it('should write a log with extra detail to kinesis', done => {
+        const logger = new ELKKinesisLogger(config);
 
-  it('should raise an error if not opened', () => {
-    const logger = new ELKKinesisLogger(config);
+        logger.open().then(() => {
+            const extraDetail = {
+                foo: 'bar'
+            };
 
-    try {
-      logger.log('test');
-    } catch (actual) {
-      assert.equal(
-        actual,
-        'ELKKinesisLogger has not been opened. Be sure to call .open() first.'
-      );
-    }
-  });
+            logger.log(logMsg, extraDetail);
 
-  it('should write a simple log to kinesis', done => {
-    const logger = new ELKKinesisLogger(config);
+            logger
+                .close()
+                .then(actual => {
+                    const expected = [
+                        {
+                            stack: 'elk-kinesis-logger',
+                            stage: 'TEST',
+                            app: 'elk-kinesis-logger-tests',
+                            timestamp: date,
+                            level: 'INFO',
+                            message: logMsg,
+                            foo: 'bar'
+                        }
+                    ];
 
-    logger.open().then(() => {
-      logger.log(logMsg);
+                    const kinesisMsg = {
+                        StreamName: config.streamName,
+                        PartitionKey: 'logs',
+                        Data: JSON.stringify(expected[0])
+                    };
 
-      logger
-        .close()
-        .then(actual => {
-          const expected = [
-            {
-              stack: 'elk-kinesis-logger',
-              stage: 'TEST',
-              app: 'elk-kinesis-logger-tests',
-              timestamp: date,
-              level: 'INFO',
-              message: logMsg
-            }
-          ];
+                    assert.equal(true, logger.kinesis.putRecord.calledOnce);
+                    assert.equal(
+                        true,
+                        logger.kinesis.putRecord.calledWith(kinesisMsg)
+                    );
 
-          const kinesisMsg = {
-            StreamName: config.streamName,
-            PartitionKey: 'logs',
-            Data: JSON.stringify(expected[0])
-          };
-
-          assert.equal(true, logger.kinesis.putRecord.calledOnce);
-          assert.equal(true, logger.kinesis.putRecord.calledWith(kinesisMsg));
-          assert.deepEqual(actual, expected);
-          done();
-        })
-        .catch(err => done(new Error(err)));
+                    assert.deepEqual(actual, expected);
+                    done();
+                })
+                .catch(err => done(new Error(err)));
+        });
     });
-  });
 
-  it('should write a log with extra detail to kinesis', done => {
-    const logger = new ELKKinesisLogger(config);
+    it('should write multiple logs', done => {
+        const logger = new ELKKinesisLogger(config);
 
-    logger.open().then(() => {
-      const extraDetail = {
-        foo: 'bar'
-      };
+        logger.open().then(() => {
+            logger.log('first message');
+            logger.log('second message');
 
-      logger.log(logMsg, extraDetail);
+            logger
+                .close()
+                .then(actual => {
+                    const expected = [
+                        {
+                            stack: 'elk-kinesis-logger',
+                            stage: 'TEST',
+                            app: 'elk-kinesis-logger-tests',
+                            timestamp: date,
+                            level: 'INFO',
+                            message: 'first message'
+                        },
+                        {
+                            stack: 'elk-kinesis-logger',
+                            stage: 'TEST',
+                            app: 'elk-kinesis-logger-tests',
+                            timestamp: date,
+                            level: 'INFO',
+                            message: 'second message'
+                        }
+                    ];
 
-      logger
-        .close()
-        .then(actual => {
-          const expected = [
-            {
-              stack: 'elk-kinesis-logger',
-              stage: 'TEST',
-              app: 'elk-kinesis-logger-tests',
-              timestamp: date,
-              level: 'INFO',
-              message: logMsg,
-              foo: 'bar'
-            }
-          ];
+                    expected
+                        .map(ex => {
+                            return {
+                                StreamName: config.streamName,
+                                PartitionKey: 'logs',
+                                Data: JSON.stringify(ex)
+                            };
+                        })
+                        .forEach(kinesisMsg => {
+                            assert.equal(
+                                true,
+                                logger.kinesis.putRecord.calledWith(kinesisMsg)
+                            );
+                        });
 
-          const kinesisMsg = {
-            StreamName: config.streamName,
-            PartitionKey: 'logs',
-            Data: JSON.stringify(expected[0])
-          };
+                    assert.equal(true, logger.kinesis.putRecord.calledTwice);
 
-          assert.equal(true, logger.kinesis.putRecord.calledOnce);
-          assert.equal(true, logger.kinesis.putRecord.calledWith(kinesisMsg));
-
-          assert.deepEqual(actual, expected);
-          done();
-        })
-        .catch(err => done(new Error(err)));
+                    assert.deepEqual(actual, expected);
+                    done();
+                })
+                .catch(err => done(new Error(err)));
+        });
     });
-  });
 
-  it('should write multiple logs', done => {
-    const logger = new ELKKinesisLogger(config);
+    it('should write logs of multiple levels', done => {
+        const logger = new ELKKinesisLogger(config);
 
-    logger.open().then(() => {
-      logger.log('first message');
-      logger.log('second message');
+        logger.open().then(() => {
+            logger.log('first message');
+            logger.error('second message');
 
-      logger
-        .close()
-        .then(actual => {
-          const expected = [
-            {
-              stack: 'elk-kinesis-logger',
-              stage: 'TEST',
-              app: 'elk-kinesis-logger-tests',
-              timestamp: date,
-              level: 'INFO',
-              message: 'first message'
-            },
-            {
-              stack: 'elk-kinesis-logger',
-              stage: 'TEST',
-              app: 'elk-kinesis-logger-tests',
-              timestamp: date,
-              level: 'INFO',
-              message: 'second message'
-            }
-          ];
+            logger
+                .close()
+                .then(actual => {
+                    const expected = [
+                        {
+                            stack: 'elk-kinesis-logger',
+                            stage: 'TEST',
+                            app: 'elk-kinesis-logger-tests',
+                            timestamp: date,
+                            level: 'INFO',
+                            message: 'first message'
+                        },
+                        {
+                            stack: 'elk-kinesis-logger',
+                            stage: 'TEST',
+                            app: 'elk-kinesis-logger-tests',
+                            timestamp: date,
+                            level: 'ERROR',
+                            message: 'second message'
+                        }
+                    ];
 
-          expected
-            .map(ex => {
-              return {
-                StreamName: config.streamName,
-                PartitionKey: 'logs',
-                Data: JSON.stringify(ex)
-              };
-            })
-            .forEach(kinesisMsg => {
-              assert.equal(
-                true,
-                logger.kinesis.putRecord.calledWith(kinesisMsg)
-              );
-            });
+                    expected
+                        .map(ex => {
+                            return {
+                                StreamName: config.streamName,
+                                PartitionKey: 'logs',
+                                Data: JSON.stringify(ex)
+                            };
+                        })
+                        .forEach(kinesisMsg => {
+                            assert.equal(
+                                true,
+                                logger.kinesis.putRecord.calledWith(kinesisMsg)
+                            );
+                        });
 
-          assert.equal(true, logger.kinesis.putRecord.calledTwice);
+                    assert.equal(true, logger.kinesis.putRecord.calledTwice);
 
-          assert.deepEqual(actual, expected);
-          done();
-        })
-        .catch(err => done(new Error(err)));
+                    assert.deepEqual(actual, expected);
+                    done();
+                })
+                .catch(err => done(new Error(err)));
+        });
     });
-  });
-
-  it('should write logs of multiple levels', done => {
-    const logger = new ELKKinesisLogger(config);
-
-    logger.open().then(() => {
-      logger.log('first message');
-      logger.error('second message');
-
-      logger
-        .close()
-        .then(actual => {
-          const expected = [
-            {
-              stack: 'elk-kinesis-logger',
-              stage: 'TEST',
-              app: 'elk-kinesis-logger-tests',
-              timestamp: date,
-              level: 'INFO',
-              message: 'first message'
-            },
-            {
-              stack: 'elk-kinesis-logger',
-              stage: 'TEST',
-              app: 'elk-kinesis-logger-tests',
-              timestamp: date,
-              level: 'ERROR',
-              message: 'second message'
-            }
-          ];
-
-          expected
-            .map(ex => {
-              return {
-                StreamName: config.streamName,
-                PartitionKey: 'logs',
-                Data: JSON.stringify(ex)
-              };
-            })
-            .forEach(kinesisMsg => {
-              assert.equal(
-                true,
-                logger.kinesis.putRecord.calledWith(kinesisMsg)
-              );
-            });
-
-          assert.equal(true, logger.kinesis.putRecord.calledTwice);
-
-          assert.deepEqual(actual, expected);
-          done();
-        })
-        .catch(err => done(new Error(err)));
-    });
-  });
 });
