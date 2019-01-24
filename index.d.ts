42121c6e0697dc3dcc7e931f22cd048770be56b1
Alex Ware <alex.ware@guardian.co.uk>
2019-01-24 15:38:45 +0000

    Add some types

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare namespace ELKKinesisLogger {
+  export interface ELKKinesisLoggerConstructorOptions { 
+    stage: string, stack: string, app: string, streamName: string, verbose: boolean = true 
+  }
+}
+
+declare class ELKKinesisLogger<ExtraDetail = Object> {
+  constructor(options: ELKKinesisLoggerConstructorOptions)
+  
+  withRole(role: string): ELKKinesisLogger
+  withProfile(profile: string): ELKKinesisLogger
+  withRegion(region: string): ELKKinesisLogger
+
+  log(message: string, extraDetail: ExtraDetail): ELKKinesisLogger
+  error(message: string, extraDetail: ExtraDetail): ELKKinesisLogger
+
+  close(): Promise<({ stage: string, stack: string, app: string, timestamp: Date; level: 'INFO' | 'ERROR'; message: string } & ExtraDetail)[]>
+}
+
+export = ELKKinesisLogger
+
